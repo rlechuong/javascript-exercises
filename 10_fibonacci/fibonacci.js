974f10c6b34bc185edda1cc47a4ea95d899fb416
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-03-14 01:34:46 -0400

    Add passing solution to the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    let fibArray = [0, 1];
+
+    for (i = 1; i < num; i++) {
+        fibArray.push(fibArray[i-1] + fibArray[i]);
+    }
+
+    return fibArray[num];
 };
 
 // Do not edit below this line

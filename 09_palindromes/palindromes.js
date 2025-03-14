2c398f0f48ccbb5be7b0f6d149249a7d6c0fb54e
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-03-14 01:10:33 -0400

    Add passing solution to the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let lowerString = string.toLowerCase().replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    let length = string.length / 2;
+    let palindrome = false;
+
+    for (i = 0; i < length; i++) {
+        if (lowerString[i] === lowerString[lowerString.length - 1 - i]) {
+            palindrome = true;
+        }
+        else {
+            palindrome = false;
+            break;
+        }
+    }
+
+    return palindrome;
 };
+
+// Solution seems much more efficient
 
 // Do not edit below this line
 module.exports = palindromes;

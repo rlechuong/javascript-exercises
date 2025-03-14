bd66a057d540b1154d63e31ab55d70ff6360d8de
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-03-14 00:47:38 -0400

    Add passing solution to the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// const arraySum = array.reduce(function(accumulator, currentValue) {
+  //   return accumulator + currentValue;
+  // }, 0);
+
+  const arraySum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+  return arraySum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // const arrayProduct = array.reduce(function (accumulator, currentValue) {
+  //   return accumulator * currentValue;
+  // })
 
+  const arrayProduct = array.reduce((accumulator, currentValue) => accumulator * currentValue);
+
+  return arrayProduct;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let numArray = [];
+
+  for (i = 0; (num - i) > 0; i++) {
+    numArray.push(num - i);
+  }
+
+  // const fact = numArray.reduce(function(accumulator, currentValue) {
+  //   return accumulator * currentValue;
+  // }, 1);
+
+  const fact = numArray.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+
+  return fact;
 };
 
 // Do not edit below this line

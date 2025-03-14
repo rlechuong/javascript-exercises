1a7fcd2ed5911f71362b30a988470c0772e80cd1
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-03-14 01:44:20 -0400

    Add passing solution to the exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    // const titles = array.map(function(book) {
+    //     return book.title;
+    // })
+
+    const titles = array.map(book => book.title);
+
+    // const titles = array.reduce(function(accumulator, currentValue) {
+    //     accumulator.push(currentValue.title);
+    //     return accumulator;
+    // }, [])
+
+    return titles;
+    
 };
 
 // Do not edit below this line
